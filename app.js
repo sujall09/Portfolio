e1c9079c099f5fb70c47b81166759dc27e8e560e
sujall09 <118647090+sujall09@users.noreply.github.com>
2023-09-09 23:57:02 +0530

    Ready
    Done with the basic page structure only the animation part is left...!

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ for (let i = 0; i < videos.length; i++) {
         videos[i].pause();
     })
 }
+
+// const snakeInfo = document.getElementById("snakeInfo")
+
+// videos[0].addEventListener("mouseenter", function (e) {
+//     snakeInfo.style.display = "block";
+//     console.log("Hello")
+// })
+// videos[0].addEventListener("mouseout", function (e) {
+//     snakeInfo.style.display = "none";
+//     console.log("Hello")
+// })
+
